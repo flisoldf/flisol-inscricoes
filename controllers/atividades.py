b9e294dfb9ed871ad3187e2e1ee57778aac997ed
Bruno Barbosa <bsbruno1@gmail.com>
2011-02-13 16:55:23 -0200

    Controller responsável pela inserção e visualização de atividades

---
+++ controllers/atividades.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+@auth.requires_membership('Palestrante')
+def index():
+    if not request.args:
+        redirect(URL('default', 'index'))
+    palestrante = request.args(0)
+    id_user = str(session.auth.user.id)
+    if palestrante == id_user:
+        ativ = db(atividade.id_usuario==palestrante).select()
+        return dict(ativ=ativ)
+    else:
+        return dict(ativ=None)
+
+
+
+@auth.requires_membership('Palestrante')
+def nova():
+    form = SQLFORM(atividade)
+    form.vars.id_usuario = session.auth.user.id
+    form.vars.status = 2
+    if form.accepts(request.vars, session):
+        session.flash = "Atividade submetida com sucesso"
+        redirect(URL('atividades', 'index', args=session.auth.user.id))
+    return dict(form=form)
