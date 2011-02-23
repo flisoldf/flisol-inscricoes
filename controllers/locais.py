1c8303a7e3ba8b4beaaa1a4ab9a0e2a4d2e303a3
Gilson Filho <contatogilsonsbf@gmail.com>
2011-02-23 14:08:19 -0300

    Controlador do modulo de Locais

---
+++ controllers/locais.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+@auth.requires_membership('Administrador')
+def lista():
+    """
+    Lista todas as salas cadastradas
+    """
+    
+    salas = db(db.sala.id > 0).select()
+    
+    return dict(salas=salas)
+    
+@auth.requires_membership('Administrador')
+def nova():
+    """
+    Invoca o formulario de cadastro
+    das salas
+    """
+    form = SQLFORM(db.sala,submit_button=T('Save'))
+    
+    if form.accepts(request.vars,session):
+        session.flash = "Sala cadastrada com sucesso"
+        redirect(URL('locais','lista'))
+    
+    return dict(form=form)
+
+@auth.requires_membership('Administrador')
+def editar():
+    """
+    Edita a sala selecionada
+    """
+    
+    # Captura seu ID
+    id_sala = request.args(0) or redirect(URL('locais','lista'))
+    
+    # Ocultando o campo ID
+    db.sala.id.readable = False
+    
+    form = SQLFORM(db.sala, id_sala,submit_button=T('Save'))
+    
+    # Renderizando o form no arquivo nova.html
+    response.view = 'locais/nova.html'
+    
+    return dict(form=form)
