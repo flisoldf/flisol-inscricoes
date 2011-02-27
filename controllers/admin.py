2d35f1d481a0a45fa3f219a280ab3df4f8b93948
Gilson Filho <contatogilsonsbf@gmail.com>
2011-02-27 20:01:54 -0300

    Retirado o plugin power Table na listagem de atividades submetidas, como tambem o metodo de alteracao de status, que foi enviado para o controller atividades

--- controllers/admin.py
+++ controllers/admin.py
@@ -13,67 +13,6 @@ def dashboard():
     
     # Consultando as atividades
     atividades = db(db.atividades.id > 0).select(orderby=db.atividades.tipo)
-   
-    # Configurando o plugin Power Table
-    table = plugins.powerTable
-    table.datasource = atividades
-    table.uitheme = 'ui-lightness'
-    table.dtfeatures['sPaginationType'] = 'scrolling'
-    table.headers = 'labels'
-    table.dtfeatures['sScrollY'] = '200'
-    table.dtfeatures['sScrollX'] = '100%'
-    table.keycolumn = 'atividades.id'
-    table.columns = ['atividades.titulo','atividades.id_usuario','atividades.nivel','atividades.tipo','atividades.materiais','atividades.status']
-    table.showkeycolumn = False
-    table.truncate = 120       
-    
-    # Consultando as atividades rejeitadas
-    rejeitadas = db(db.atividades.status == 'Rejeitada').select()
     
     # return dict(atividades = atividades)
-    return dict(atividades = table.create())
-
-
-@auth.requires_membership('Administrador')
-def status():
-    id_atividade = request.args(0) or redirect(URL('admin','dashboard'))
-
-    # Exibindo somente o campo status
-    db.atividades.id.readable = False
-
-    db.atividades.titulo.writable = False
-
-    db.atividades.descricao.readable = \
-    db.atividades.descricao.writable = False
-
-    db.atividades.nivel.readable = \
-    db.atividades.nivel.writable = False
-
-    db.atividades.tipo.readable = \
-    db.atividades.tipo.writable = False
-
-    db.atividades.duracao.readable = \
-    db.atividades.duracao.writable = False
-
-    db.atividades.tag.readable = \
-    db.atividades.tag.writable = False
-
-    db.atividades.arquivo.readable = \
-    db.atividades.arquivo.writable = False
-
-    db.atividades.materiais.readable = \
-    db.atividades.materiais.writable = False
-
-    db.atividades.observacoes.readable = \
-    db.atividades.observacoes.writable = False
-
-    db.atividades.checa_apresentacao.readable = \
-    db.atividades.checa_apresentacao.writable = False
-
-    atividade = SQLFORM(db.atividades, id_atividade, submit_button=T('Save'),formstyle='table3cols')
-    
-    if atividade.accepts(request.vars, session):
-        session.flash = "Status alterado com sucesso"
-        redirect(URL('admin','dashboard'))
-
-    return dict(atividade = atividade)
+    return dict(atividades = atividades)
